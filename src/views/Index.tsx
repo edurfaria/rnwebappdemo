9acd62cece538d024735dbde0decbeb4c564c597
DESKTOP-AM7UKF8\Eduardo <efaria@hes-inovacao.com>
2022-09-28 17:44:01 +0100

    Index View

---
+++ src/views/Index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native'
+
+interface IIndexComponentProps {
+}
+
+const IndexComponent: React.FunctionComponent<IIndexComponentProps> = (props) => {
+    return (
+        <View>
+            <Text>És mesmo piu piu</Text>
+        </View>
+    );
+};
+
+export default IndexComponent;
