6b1cc9a335aa24cc0ef8538bedbcf7e1cff81278
DESKTOP-AM7UKF8\Eduardo <efaria@hes-inovacao.com>
2022-09-29 16:02:45 +0100

    Open Sans Added

--- src/theme/Fonts.ts
+++ src/theme/Fonts.ts
@@ -1,35 +1,22 @@
 const localFonts = {
-/*     //Inter
-    "Inter-Bold": require('../../assets/fonts/Inter/Inter-Bold.ttf'),
-    "Inter-SemiBold": require('../../assets/fonts/Inter/Inter-SemiBold.ttf'),
-    "Inter-Medium": require('../../assets/fonts/Inter/Inter-Medium.ttf'),
-    "Inter-Regular": require('../../assets/fonts/Inter/Inter-Regular.ttf'),
-    "Inter-Thin": require('../../assets/fonts/Inter/Inter-Thin.ttf'),
     //OpenSans
     "OpenSans-ExtraBold": require('../../assets/fonts/OpenSans/OpenSans-ExtraBold.ttf'),
     "OpenSans-Bold": require('../../assets/fonts/OpenSans/OpenSans-Bold.ttf'),
     "OpenSans-SemiBold": require('../../assets/fonts/OpenSans/OpenSans-SemiBold.ttf'),
     "OpenSans-Medium": require('../../assets/fonts/OpenSans/OpenSans-Medium.ttf'),
     "OpenSans-Regular": require('../../assets/fonts/OpenSans/OpenSans-Regular.ttf'),
-    "OpenSans-Light": require('../../assets/fonts/OpenSans/OpenSans-Light.ttf'), */
+    "OpenSans-Light": require('../../assets/fonts/OpenSans/OpenSans-Light.ttf'),
 }
 
 const family = {
-    /*     inter: {
-            bold: "Inter-Bold",
-            semi: "Inter-SemiBold",
-            medium: "Inter-Medium",
-            regular: "Inter-Regular",
-            thin: "Inter-Thin",
-        },
-        opensans: {
-            extrabold: "OpenSans-ExtraBold",
-            bold: "OpenSans-Bold",
-            semibold: "OpenSans-SemiBold",
-            medium: "OpenSans-Medium",
-            regular: "OpenSans-Regular",
-            light: "OpenSans-Light",
-        } */
+    opensans: {
+        extrabold: "OpenSans-ExtraBold",
+        bold: "OpenSans-Bold",
+        semibold: "OpenSans-SemiBold",
+        medium: "OpenSans-Medium",
+        regular: "OpenSans-Regular",
+        light: "OpenSans-Light",
+    },
 };
 
 const Fonts = { localFonts, family };
