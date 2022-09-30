e77ae3d02a81b2b2b692b006ac3feaa789a82c21
DESKTOP-AM7UKF8\Eduardo <efaria@hes-inovacao.com>
2022-09-30 15:35:07 +0100

    Added Theme Styles and Colors Files

---
+++ src/theme/Colors.ts
@@ -0,0 +1,12 @@
+const Colors = {
+    pallete: {
+        primary: '#318EFE',
+    },
+    text: {
+        header: '#fff',
+        primary: '#333',
+        sencudaty: '#4F5B65'
+    }
+}
+
+export default Colors 
---
+++ src/theme/Styles.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native'
+import Colors from './Colors'
+
+export const globalStyles = StyleSheet.create({
+    CustomDrawerContent: {
+        flex: 1,
+        backgroundColor: Colors.pallete.primary,
+    },
+})
